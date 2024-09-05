64495ea8df0fe2eaa478a6813f43389d544b1546
Oscar Yamada <149902639+OscarYamada@users.noreply.github.com>
2024-09-04 23:45:36 -0700

    Update script.js
    vscode won't commit, so i will

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer", "YouTuber", "Blogger"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
+    strings: ["Mechanical Engineer", "Robotics Specialist", "Coder and Builder"],
+    typeSpeed: 75,
+    backSpeed: 75,
+    backDelay: 1600,
     loop: true
 })
